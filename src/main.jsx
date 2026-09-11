98c033c89c1df4157c1981ed97b7e322e5a1f800
Thamizamudhan K <249441483+tk1573-sys@users.noreply.github.com>
2026-09-12 00:45:35 +0530

    Wire PhD command centre into Life Centre shell

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import PhdCommandCenter from './PhdCommandCenter.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <PhdCommandCenter />
   </StrictMode>
 )
